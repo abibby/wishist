84c9ce952fc577ed86eb7db4f2d5a68291d3c9f7
Adam Bibby <Adam_Bibby@hotmail.com>
2024-06-04 07:45:40 -0400

    add type check to ci (#31)
    Co-authored-by: Adam Bibby <abibby@ownersbox.com>

--- ui/src/components/button-list.tsx
+++ ui/src/components/button-list.tsx
@@ -3,6 +3,6 @@ import styles from './button-list.module.css'
 
 h
 
-export function ButtonList({ children }: RenderableProps<never>) {
+export function ButtonList({ children }: RenderableProps<{}>) {
     return <section class={styles.buttonList}>{children}</section>
 }
