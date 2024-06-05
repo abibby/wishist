84452c7db8ff584a839ab6b79336681aec115736
Adam Bibby <abibby@ownersbox.com>
2024-06-05 07:37:41 -0400

    Allow for env

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import preact from '@preact/preset-vite'
+import { resolve } from 'path'
 
-export default defineConfig({
-    plugins: [preact()],
-    // build: {
-    //     minify: false,
-    // },
-    server: {
-        port: 12345,
-        proxy: {
-            '/api': 'http://localhost:32148',
+export default defineConfig(({ mode }) => {
+    const env = {
+        ...process.env,
+        ...loadEnv(mode, resolve(process.cwd(), '..')),
+    }
+
+    return {
+        plugins: [preact()],
+        server: {
+            port: 12345,
+            proxy: {
+                '/api': env.VITE_PROXY_HOST ?? 'http://localhost:32148',
+            },
         },
-    },
+    }
 })
